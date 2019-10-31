148068f8a0467d01b9c5f08beb1dd8e6594d03e3
dvrzan <dvrzan@icloud.com>
2019-10-31 16:02:56 -0400

    add Storyboard navigation bars

---
+++ Analyzar/Controllers/HomeTabBarViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HomeTabBarViewController.swift
+//  Analyzar
+//
+//  Created by Danijela Vrzan on 2019-10-31.
+//  Copyright © 2019 Danijela Vrzan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let isDarkMode = UserDefaults.standard.bool(forKey: "isDarkMode")
+             
+        if isDarkMode == true {
+            if #available(iOS 13.0, *) {
+                overrideUserInterfaceStyle = .dark
+            } else {
+                     // Fallback on earlier versions
+            }
+        } else {
+            if #available(iOS 13.0, *) {
+                overrideUserInterfaceStyle = .light
+            } else {
+                // Fallback on earlier versions
+            }
+        }
+        
+    }
+    
+
+}
--- Analyzar/Controllers/OnboardingViewController.swift
+++ Analyzar/Controllers/OnboardingViewController.swift
@@ -27,22 +27,6 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
         pageControl.currentPage = 0
         view.bringSubviewToFront(pageControl)
         
-        let isDarkMode = UserDefaults.standard.bool(forKey: "isDarkMode")
-        
-        if isDarkMode == true {
-            if #available(iOS 13.0, *) {
-                overrideUserInterfaceStyle = .dark
-            } else {
-                // Fallback on earlier versions
-            }
-        } else {
-            if #available(iOS 13.0, *) {
-                overrideUserInterfaceStyle = .light
-            } else {
-                // Fallback on earlier versions
-            }
-        }
-        
     }
     
     func createSlides() -> [Slide] {
